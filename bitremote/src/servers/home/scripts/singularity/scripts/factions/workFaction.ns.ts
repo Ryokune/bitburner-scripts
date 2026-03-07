c7d5c2c36333570bdcbb5831169f37a560b3cb2c
fish <kiokunee@gmail.com>
2026-03-07 20:08:11 +0800

    change calculation for workFaction money

--- bitremote/src/servers/home/scripts/singularity/scripts/factions/workFaction.ns.ts
+++ bitremote/src/servers/home/scripts/singularity/scripts/factions/workFaction.ns.ts
@@ -1,7 +1,8 @@
 import type { FactionWorkTask } from "@ns"
 
 export async function main(ns: NS) {
-  const factions = ns.getPlayer().factions
+  const player = ns.getPlayer()
+  const factions = player.factions
   const owned = ns.singularity.getOwnedAugmentations(true)
 
   const work = ns.singularity.getCurrentWork()
@@ -20,13 +21,15 @@ export async function main(ns: NS) {
       if (augmentation === "NeuroFlux Governor") continue
       if (owned.includes(augmentation)) continue
 
+      // player faction rep doesnt go down (i think at least)
+      // but money does. remove aug - player.money if this causes any weird issues.
       const augmentation_rep = ns.singularity.getAugmentationRepReq(augmentation) -
         ns.singularity.getFactionRep(faction)
-      const augmentation_cost = ns.singularity.getAugmentationPrice(augmentation)
+      const augmentation_cost = ns.singularity.getAugmentationPrice(augmentation) - player.money
 
       if (
         augmentation_rep < lowest_reputation_req ||
-        (augmentation_rep === lowest_reputation_req && augmentation_cost < lowest_cost)
+        (augmentation_rep <= lowest_reputation_req && augmentation_cost < lowest_cost)
       ) {
         lowest_reputation_req = augmentation_rep
         lowest_cost = augmentation_cost
@@ -34,12 +37,12 @@ export async function main(ns: NS) {
       }
     }
   }
-
   if (!cheapest_faction) return
 
   if (work?.type === "FACTION" && work.factionName === cheapest_faction) return
 
   if (ns.singularity.workForFaction(cheapest_faction, "hacking")) {
     ns.toast(`Doing work for ${cheapest_faction}`)
+    ns.tprint(`Doing work for ${cheapest_faction}`)
   }
 }
