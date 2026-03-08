35c96b2b2293742756f60c98ea86f66c79d4467b
fish <kiokunee@gmail.com>
2026-03-09 07:25:31 +0800

    make create program a tad bit more robust

--- bitremote/src/servers/home/scripts/singularity/scripts/work/_createProgram.ns.ts
+++ bitremote/src/servers/home/scripts/singularity/scripts/work/_createProgram.ns.ts
@@ -3,6 +3,7 @@ import { PROGRAMS } from "@home/lib/main"
 export async function main(ns: NS) {
   // Probably the only two most practical ones to create. Others you can just buy.. maybe not in other bitnodes though, we will see.
   for (const program of ["BruteSSH.exe", "FTPCrack.exe"]) {
-    ns.singularity.createProgram(program, true)
+    ns.singularity.createProgram(program, ns.args[0] as boolean)
+    await ns.sleep(1000)
   }
 }
--- bitremote/src/servers/home/scripts/singularity/scripts/work/program.ns.ts
+++ bitremote/src/servers/home/scripts/singularity/scripts/work/program.ns.ts
@@ -2,11 +2,13 @@ import { runScript } from "@home/lib/main"
 
 export async function main(ns: NS) {
   const work = ns.singularity.getCurrentWork()
-  if (work && (work.type != "CLASS" && work.type != "FACTION")) {
+  if (work && (work.type != "CLASS" && work.type != "FACTION" && work.type != "CREATE_PROGRAM")) {
     ns.toast(`Not running program task. Currently has work. ${work.type}`, "warning")
     return
   }
-  if (runScript(ns, "../work/_createProgram.ns.js") == 0) {
+  if (runScript(ns, "../work/_createProgram.ns.js", {
+    preventDuplicates: true
+  }, work?.type !== "CREATE_PROGRAM") == 0) {
     ns.toast("Could not run create program task :(", "error")
   }
 }
