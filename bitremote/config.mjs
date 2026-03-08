134cc9f714e9cb831e4f245fa30c2d8c2a0d1fde
fish <kiokunee@gmail.com>
2026-03-08 09:20:44 +0800

    feat: add import resolver for ingame *.ns.ts imports
    this commit makes ingame `*.ns.ts` imports get resolved into `*.ns.js` and not bundled, as its already in the game

--- bitremote/config.mjs
+++ bitremote/config.mjs
@@ -1,5 +1,44 @@
 import { context } from 'esbuild';
 import { BitburnerPlugin } from 'esbuild-bitburner-plugin';
+import path from 'path'
+
+const bitburnerPathPlugin = {
+  name: 'bitburner-path-remapper',
+  setup(build) {
+    build.onResolve({ filter: /.*/ }, async (args) => {
+      if (args.pluginData?.bbskip) return null;
+      const result = await build.resolve(args.path, {
+        resolveDir: args.resolveDir,
+        kind: args.kind,
+        pluginData: { bbskip: true }
+      });
+
+      if (result.errors.length > 0) return { errors: result.errors };
+      if (result.path.endsWith('.ns.ts')) {
+        let relativePath = path.relative(args.resolveDir, result.path);
+
+        relativePath = relativePath.replace(/\.tsx?$/, '.js');
+
+        let finalPath = relativePath.split(path.sep).join('/');
+        if (!finalPath.startsWith('.')) {
+          finalPath = './' + finalPath;
+        }
+
+        console.log(`[REMAPPED] ${args.path} -> ${finalPath} (${args.importer})`);
+
+        return {
+          path: finalPath,
+          external: true
+        };
+      }
+
+
+      // 3. If it's a normal .ts file or anything else, return null 
+      // This tells esbuild: "Proceed with normal bundling"
+      return null;
+    });
+  },
+};
 
 const createContext = async () => await context({
   entryPoints: [
@@ -23,7 +62,7 @@ const __doc = eval("document");
     document: "__doc"
   },
   plugins: [
-
+    bitburnerPathPlugin,
     BitburnerPlugin({
       port: 12525,
       types: 'NetscriptDefinitions.d.ts',
