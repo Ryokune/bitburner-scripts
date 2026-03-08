ad9e51ee21cecdca876441ed448c1abc158eec23
fish <kiokunee@gmail.com>
2026-03-09 01:46:06 +0800

    Set createProgram to only create BruteSSH and FTPCrack

--- bitremote/src/servers/home/scripts/singularity/scripts/work/_createProgram.ns.ts
+++ bitremote/src/servers/home/scripts/singularity/scripts/work/_createProgram.ns.ts
@@ -1,7 +1,8 @@
 import { PROGRAMS } from "@home/lib/main"
 
 export async function main(ns: NS) {
-  for (const program of Object.keys(PROGRAMS)) {
+  // Probably the only two most practical ones to create. Others you can just buy.. maybe not in other bitnodes though, we will see.
+  for (const program of ["BruteSSH.exe", "FTPCrack.exe"]) {
     ns.singularity.createProgram(program, true)
   }
 }
