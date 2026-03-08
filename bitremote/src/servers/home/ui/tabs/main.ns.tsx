c24b2c9493d5f5d00127c4055c7d08937e1667f8
fish <kiokunee@gmail.com>
2026-03-09 02:36:57 +0800

    tabs: automatically open in x 700 instead of 0

--- bitremote/src/servers/home/ui/tabs/main.ns.tsx
+++ bitremote/src/servers/home/ui/tabs/main.ns.tsx
@@ -2,7 +2,7 @@ import { CreateWindow } from "@home/lib/ui"
 import { App } from "./App"
 
 export async function main(ns: NS) {
-  CreateWindow(ns, () => <App ns={ns} />, "Tail Processes", 150, 500, 0, 0)
+  CreateWindow(ns, () => <App ns={ns} />, "Tail Processes", 150, 500, 700, 0)
   while (ns.getRunningScript()?.tailProperties) {
     await ns.asleep(1000)
   }
