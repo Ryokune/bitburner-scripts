f9be948202be6f8e445c7b7d38e842606a82f132
fish <kiokunee@gmail.com>
2026-03-09 22:51:00 +0800

    add exclude flag to kill all command

--- bitremote/src/servers/home/scripts/kill_all.ns.ts
+++ bitremote/src/servers/home/scripts/kill_all.ns.ts
@@ -1,13 +1,18 @@
-import { getHosts } from "@home/lib/main"
-
+import { Flags, getHosts } from "@home/lib/main"
+const FLAGS: Flags = [
+  ["exclude", [""]]
+]
 export async function main(ns: NS) {
-  for (const host of getHosts(ns)) {
+  const flags = ns.flags(FLAGS)
+  const exclude = new Set(flags.exclude as string[])
+  for (const host of getHosts(ns, h => !exclude.has(h))) {
     ns.killall(host, true)
     await ns.sleep(10)
   }
 }
 
 export function autocomplete(data: AutocompleteData, args: string[]): string[] {
+  data.flags(FLAGS)
   return []
 }
 
